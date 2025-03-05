322e910f59b5b398105a146e23ba439801f3c300
Ruirui Jia <bl24729@bristol.ac.uk>
2025-03-05 02:08:08 +0000

    update demo2.1 preload url

--- demo2.1/src/ui/AssetManager.js
+++ demo2.1/src/ui/AssetManager.js
@@ -6,30 +6,30 @@ export class AssetManager {
   
     preload() {
       // background image
-      this.images["mainBg"] = loadImage("../assets/mainBg.jpg");
+      this.images["mainBg"] = loadImage("./assets/mainBg.jpg");
   
       // sounds
-      this.sounds["bgMusic"] = loadSound("../assets/Cute and chu-2 byo.mp3");
+      this.sounds["bgMusic"] = loadSound("./assets/Cute and chu-2 byo.mp3");
   
       // player images
-      this.images["player1"] = loadImage("../assets/Character1_Walking.png");
-        this.images["player2"] = loadImage("../assets/Character2_Walking.png");
+      this.images["player1"] = loadImage("./assets/Character1_Walking.png");
+        this.images["player2"] = loadImage("./assets/Character2_Walking.png");
       // item images
-      this.images["cargo1"] = loadImage("../assets/delivery-box-45.png");
+      this.images["cargo1"] = loadImage("./assets/delivery-box-45.png");
       //car images
-      this.images["car1"] = loadImage("../assets/s-blue-van.svg");
-      this.images["car2"] = loadImage("../assets/南-轿车-红.png");
-      this.images["car3"] = loadImage("../assets/南-公交车-橙.png");
-      this.images["car4"] = loadImage("../assets/s-blue-truck.svg");
-      this.images["car5"] = loadImage("../assets/s-darkBlue-van.svg");
-      this.images["car6"] = loadImage("../assets/s-grey-saloon.svg");
-      this.images["car7"] = loadImage("../assets/s-white-truck-2.png");
+      this.images["car1"] = loadImage("./assets/s-blue-van.svg");
+      this.images["car2"] = loadImage("./assets/南-轿车-红.png");
+      this.images["car3"] = loadImage("./assets/南-公交车-橙.png");
+      this.images["car4"] = loadImage("./assets/s-blue-truck.svg");
+      this.images["car5"] = loadImage("./assets/s-darkBlue-van.svg");
+      this.images["car6"] = loadImage("./assets/s-grey-saloon.svg");
+      this.images["car7"] = loadImage("./assets/s-white-truck-2.png");
   
       //volume images
-      this.images["volumeOn"] = loadImage("../assets/speaker_normal.svg");
-      this.images["volumeOff"] = loadImage("../assets/speaker-off_normal.svg");
+      this.images["volumeOn"] = loadImage("./assets/speaker_normal.svg");
+      this.images["volumeOff"] = loadImage("./assets/speaker-off_normal.svg");
       //pause image
-      this.images["pause"] = loadImage("../assets/pause-button_normal.svg");
+      this.images["pause"] = loadImage("./assets/pause-button_normal.svg");
   
     }
   
